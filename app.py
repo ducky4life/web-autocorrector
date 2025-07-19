9468cd739c825154b3838ebd218bfcc5938d6a84
Ducky <124077142+ducky4life@users.noreply.github.com>
2025-07-20 06:24:57 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, render_template, send_from_directory
+from threading import Thread
+from waitress import serve
+from autocorrector import prettify_autocorrector
+
+app = Flask('')
+
+@app.route('/', methods=['GET', 'POST'])
+def main_route():
+    message = ""
+
+    if request.method == 'POST':
+        query = request.form.get('textbox', '')
+        action = request.form.get('action')
+        number = int(action)
+
+        if query:
+            try:
+                message = prettify_autocorrector(query, number)
+
+            except Exception as e:
+                message = f"Error: {e}"
+        else:
+            message = "Please enter a query."
+            
+    return render_template("index.html", message=message)
+
+def run():
+    serve(app, host="0.0.0.0", port=8080)
+
+def keep_alive():
+    server = Thread(target=run)
+    server.start()
+    print("server is running on port 8080")
+
+@app.route('/downloads/<path:filename>')
+def download_file(filename):
+    return send_from_directory('downloads', filename, as_attachment=True)
+
+keep_alive()
---
+++ autocorrector.py
@@ -0,0 +1,30 @@
+from dyslexicloglog import Autocorrector
+
+ac = Autocorrector()
+
+def autocorrector(query:str, number:int=1):
+    input_list = query.split(",")
+    if number not in [1,2,3]:
+        return "please choose a number between 1 to 3 inclusive"
+    
+    ac_results = ac.top3(input_list)
+
+    if number == 3:
+        return ac_results
+    else:
+        for key in ac_results:
+            for i in range(3-number):
+                ac_results[key].pop(-1)
+        return ac_results
+
+def prettify_autocorrector(query:str, number:int=1):
+    ac_results = autocorrector(query, number)
+    msg = ""
+    for key in ac_results:
+        output = []
+        word_list = ac_results[key.lower()]
+
+        for i in range(1, len(word_list)+1):
+            output.append(f"{i}. {word_list[i-1]}")
+        msg += f'{key}: {" ".join(output)}\n'
+    return msg
