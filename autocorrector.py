524f1d78edd2d6ab3b4ac15f9aa10cd880b40845
Ducky <124077142+ducky4life@users.noreply.github.com>
2025-07-20 07:01:18 +0800

    newline

--- autocorrector.py
+++ autocorrector.py
@@ -26,5 +26,5 @@ def prettify_autocorrector(query:str, number:int=1):
 
         for i in range(1, len(word_list)+1):
             output.append(f"{i}. {word_list[i-1]}")
-        msg += f'{key}: {" ".join(output)}'
+        msg += f'{key}: {" ".join(output)}\n'
     return msg
