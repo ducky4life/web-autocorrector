b715d6342b04beaaa084d34c2aa4cc5db5c11492
Ducky <124077142+ducky4life@users.noreply.github.com>
2025-07-20 07:06:48 +0800

    lower for now

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def main_route():
 
         if query:
             try:
-                message = prettify_autocorrector(query, number)
+                message = prettify_autocorrector(query.lower(), number)
 
             except Exception as e:
                 message = f"Error: {e}"
