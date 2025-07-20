40c9a19787c306d9782c9edc30288f414038f601
Ducky <124077142+ducky4life@users.noreply.github.com>
2025-07-21 05:42:01 +0800

    input file

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, Response, request, render_template, jsonify
+from flask import Flask, request, render_template, jsonify
 from threading import Thread
 from waitress import serve
 import requests
@@ -14,21 +14,34 @@ def main_route():
     output_file_name = ""
 
     if request.method == 'POST':
-        query = request.form.get('textbox', '')
+
+        
+        input_text = request.form.get('input', '')
+        input_file = request.files.get("input_file_upload")
+
+        if input_file:
+            input_file_content = input_file.readlines()
+            query = [item.decode('utf-8').replace("\n", "") for item in input_file_content][0]
+        
+        else:
+            query = input_text
+
+
 
         action = request.form.get('action')
         number = int(action)
         
+
+
         dictionary_input = request.form.get('dictionary', '')
         dictionary_file = request.files.get("dictionary_file_upload")
 
+
         if dictionary_input.startswith("http"): # url from web
-            
             url_content = requests.get(dictionary_input, params={"downloadformat": "txt"}).text
             dictionary = url_content.split("\n")
 
         elif dictionary_file: # uploaded file
-
             dictionary_file_content = dictionary_file.readlines()
             dictionary = [item.decode('utf-8').replace("\n", "") for item in dictionary_file_content]
                 
@@ -38,19 +51,20 @@ def main_route():
         else:
             dictionary = "test_files/20k_shun4midx.txt"
 
+
+
         if query:
             output_as_file = request.form.get('output_file_toggle') # 'on' or None
 
             if output_as_file == "on":
 
-                output_file_name = f"{int(time.time())}.txt"
+                output_file_name = f"fqhll_output_{int(time.time())}.txt"
                 content = autocorrector(query, number, dictionary)
                 response = jsonify(content)
                 response.headers["Content-Disposition"] = f"attachment; filename={output_file_name}"
                 return response
 
             else:
-
                 try:
                     message = prettify_autocorrector(query, number, dictionary)
 
