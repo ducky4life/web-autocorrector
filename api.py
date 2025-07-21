2b4baa098bd74cc75edc6d0c636e9bbca5657692
Ducky <124077142+ducky4life@users.noreply.github.com>
2025-07-21 22:55:23 +0800

    Update api.py

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, current_app
 from threading import Thread
 from waitress import serve
 from flask_restful import Resource, Api, reqparse
@@ -10,7 +10,7 @@ parser.add_argument('query', type=str, help='query to autocorrect', location='fo
 parser.add_argument('number', type=int, help='number of autocorrect suggestions to output', location='form')
 parser.add_argument('dictionary', type=str, help='dictionary from web url or path to file on server', location='form')
 parser.add_argument('separator', type=str, help='separator for words. defaults to spaces', location='form')
-# parser.add_argument('prettify', type=bool, help='whether to make the json output indented', location='form')
+parser.add_argument('prettify', type=bool, help='whether to make the json output indented', location='form')
 
 def request_url_to_list(url):
     url_content = requests.get(url, params={"downloadformat": "txt"}).text
@@ -23,12 +23,15 @@ class AutocorrectorApi(Resource):
         number = args['number'] if args['number'] else 1
         dictionary = request_url_to_list(args['dictionary']) if args['dictionary'] else "test_files/20k_shun4midx.txt"
         separator = args['separator'] if args['separator'] else " "
-        # prettify = args['prettify'] if args['prettify'] else False
+        prettify = args['prettify'] if args['prettify'] else False
 
         if args['query'].startswith("http"):
             query = request_url_to_list(args['query'])
         elif args['query']:
             query = query.split(separator)
+
+        if prettify == True:
+            current_app.json.compact = False
 
         print(query)
 
