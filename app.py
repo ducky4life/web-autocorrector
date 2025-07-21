a72766a71ab1582d3a3823153f7857c16cfdf0f2
Ducky <124077142+ducky4life@users.noreply.github.com>
2025-07-21 19:14:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from api import AutocorrectorApi
 app = Flask('')
 port = 8080
 api_app = Api(app)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 def request_url_to_list(url):
     url_content = requests.get(url, params={"downloadformat": "txt"}).text
